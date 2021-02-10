dd14212f79067db81c4a353713b12fb34f8ed526
Dellaa <78464022+adelliamaharanip@users.noreply.github.com>
2021-02-10 14:54:56 +0700

    Tugas 1 Pemrograman Python

---
+++ PROGRAMs 10.py
@@ -0,0 +1,9 @@
+#Mengkonversi suhu celcius kedalam reamur dan fahrenheit
+
+celcius = int(input('input suhu celcius : '))
+
+reamur = celcius * 4/5
+fahrenheit = (celcius * 9/5) + 32
+
+print(celcius, 'dikonversi dalam reamur menjadi ', reamur)
+print(celcius, 'dikonversi dalam fahrenheit menjadi ', fahrenheit)
