6aa7fcc1df261ff31f8526faafcdf3020d21ed6b
Dellaa <78464022+adelliamaharanip@users.noreply.github.com>
2021-02-24 13:57:54 +0700

    Delete PROGRAM 6.py

--- PROGRAM 6.py
+++
@@ -1,9 +0,0 @@
-kata = 'nama saya adellia maharani putri'
-
-jum = 0
-for letter in kata:
-    if letter == 'a':
-        jum += 1
-        #continue
-    print('huruf sekarang : ', letter)
-print('jumlah : ', jum)
